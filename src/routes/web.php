49a82286ac66e9d1605a27e0d1fe43a7c62fd93c
Daniel <daniel.olivqueiroz@outlook.com>
2025-06-09 09:30:30 -0300

    feat: 15 route grouping

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,19 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/** Route Grouping */
 
-Route::get('about', function () {
-    return "<h1>About Page</h1>";
-})->name('hello');
+Route::group(['prefix' => 'user'], function() {
+    Route::get('/', function() {
+    return "<h1>Customer List</h1";
+    });
 
-Route::get('contact', function () {
-    return "<h1>Contact Page</h1>";
+    Route::get('/create', function() {
+    return "<h1>Customer Create</h1";
+    });
+
+    Route::get('/show', function() {
+    return "<h1>Customer Show</h1";
+    });
 });
 
-Route::get('contact/{id}', function ($id) {
-    return $id;
-})->name('edit-contact');
 
-Route::get('home', function() {
-    return "<a href='".route('edit-contact', 1)."'>About</a>";
-});
