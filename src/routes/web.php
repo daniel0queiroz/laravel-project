4e5c8e51aacdadad9777adc6c167b8f5ec77fe0d
Daniel <daniel.olivqueiroz@outlook.com>
2025-06-09 09:22:44 -0300

    feat: 14 naming routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('about', function () {
+    return "<h1>About Page</h1>";
+})->name('hello');
+
+Route::get('contact', function () {
+    return "<h1>Contact Page</h1>";
+});
+
+Route::get('contact/{id}', function ($id) {
+    return $id;
+})->name('edit-contact');
+
+Route::get('home', function() {
+    return "<a href='".route('edit-contact', 1)."'>About</a>";
+});
