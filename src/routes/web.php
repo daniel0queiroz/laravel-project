515fb4950f73864819af0e11a7a977c103accd5f
Daniel <daniel.olivqueiroz@outlook.com>
2025-06-14 11:34:53 -0300

    feat: 27 resource controllers

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +26,8 @@ Route::get('/home', [HomeController::class, 'index']);
 Route::get('about', [AboutController::class, 'index'])->name('about');
 
 Route::get('contact', [ContactController::class, 'index']);
+
+Route::resource('blog', BlogController::class);
 
 /**
  * M V C
