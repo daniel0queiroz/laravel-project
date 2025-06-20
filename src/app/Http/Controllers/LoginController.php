9af384566d35ed184d3c4e6ef654576fdccb08ff
Daniel <daniel.olivqueiroz@outlook.com>
2025-06-20 10:45:44 -0300

    feat: 33 overwrite default validation texts

--- src/app/Http/Controllers/LoginController.php
+++ src/app/Http/Controllers/LoginController.php
@@ -17,6 +17,11 @@ class LoginController extends Controller
             'name' => ['required', 'alpha', 'min: 6', 'max: 10'],
             'email' => ['required', 'email'],
             'password' => 'required' 
+        ], [
+            'name.required' => 'The user name field is required.',
+            'name.alpha' => 'User name should only contain letters.',
+            'email.email' => 'Hello this is not a email.',
+            'name.min' => 'Must be 6 caractheres.'
         ]);
 
         return $request;
