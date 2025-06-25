04d9f89a08e7fdc8e23bb22fb299c2a8e38eadd0
Daniel <daniel.olivqueiroz@outlook.com>
2025-06-25 09:41:23 -0300

    feat: 42 retriving a list of column values

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -11,28 +12,8 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-       $blogs = [
-        [
-            'title' => 'Title one',
-            'body' => 'this is a body text',
-            'status' => 0
-        ],
-        [
-            'title' => 'Title two',
-            'body' => 'this is a body text',
-            'status' => 0
-        ],
-        [
-            'title' => 'Title three',
-            'body' => 'this is a body text',
-            'status' => 1
-        ],
-        [
-            'title' => 'Title four',
-            'body' => 'this is a body text',
-            'status' => 0
-        ]
-        ];
-    return view('home', compact('blogs'));
+        return DB::table('posts')->first();
+
+        return view('home', compact('blogs'));
     }
 }
