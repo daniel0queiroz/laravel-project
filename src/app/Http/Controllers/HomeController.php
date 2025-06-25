94097a76f4b9a1b4e4145a2164dcd68afe7573e1
Daniel <daniel.olivqueiroz@outlook.com>
2025-06-25 10:06:21 -0300

    feat: 45 insert data in database

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -12,8 +12,23 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return DB::table('posts')->where('status', '=', 1)->get();
+        DB::table('posts')->insert([
+            [
+            'title' => 'This is a test Data',
+            'description' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum reprehenderit dicta explicabo autem aut magnam dignissimos voluptates nisi quis assumenda incidunt dolorem praesentium, dolore sint possimus voluptatem nostrum expedita quos.',
+            'status' => 1,
+            'publish_date' => date('Y-m-d'),
+            'user_id' => 1
+            ],
+            [
+            'title' => 'This is a test Data 2',
+            'description' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum reprehenderit dicta explicabo autem aut magnam dignissimos voluptates nisi quis assumenda incidunt dolorem praesentium, dolore sint possimus voluptatem nostrum expedita quos.',
+            'status' => 1,
+            'publish_date' => date('Y-m-d'),
+            'user_id' => 1
+        ]
+        ]);
 
-        return view('home', compact('blogs'));
+        dd('success');
     }
 }
