5fecd3f966f32ba2562518b54c10312fe88f541d
Daniel <daniel.olivqueiroz@outlook.com>
2025-06-26 11:04:13 -0300

    feat: 52 using where condition in eloquent

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -14,20 +14,7 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-       /**
-        * 1. Retrieving all data
-        * 2. Retrieving single data
-        * 3. Retrieving a single data or throw error
-        */
-
-        // return Post::all(); // DB::table('posts')->get()
-
-        // return $post = Post::findOrFail(4);
-
-        $posts = Post::all();
-
-        foreach($posts as $post) {
-            echo $post->title;
-        }
+    //    return Post::where('views', '>', 100)->where('id', '=', 2)->get();
+       return Post::where('views', '>', 400)->orWhere('id', '=', 2)->get();
     }
 }
