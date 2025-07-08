ecb582d76e92c123452127e5f1a4cb6e7f50fd1d
Daniel <daniel.olivqueiroz@outlook.com>
2025-07-08 10:41:08 -0300

    feat: 77 creating models and migrations for crud

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -4,34 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory, SoftDeletes;
-
-    // fillable
-    // guarded
-
-    protected $fillable = [
-        'title',
-        'description',
-        'status',
-        'publish_date',
-        'user_id',
-        'category_id',
-        'views'
-    ];
-
-    // protected $guarded = [];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function tags()
-    {
-        return $this->belongsToMany(Tag::class);
-    }
+    use HasFactory;
 }
