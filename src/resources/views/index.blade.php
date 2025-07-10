fb3c40061c9f474f625e70e24281dc1960d7d2ec
Daniel <daniel.olivqueiroz@outlook.com>
2025-07-10 09:13:37 -0300

    feat: 80 retrieving all data from database table

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -28,22 +28,23 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <th scope="row">{{$post->id}}</th>
                                 <td>
-                                    <img src="https://picsum.photos/200" alt="" width="80">
+                                    <img src="{{asset($post->image)}}" alt="" width="80">
                                 </td>
-                                <td>Lorem ipsum dolor sit amet</td>
-                                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus odit, cumque explicabo.</td>
-                                <td>News</td>
-                                <td>2-5-23</td>
-                                <td>
-                                    <a class="btn-sm btn-success btn" href="">Show</a>
-                                    <a class="btn-sm btn-primary btn" href="">Edit</a>
-                                    <a class="btn-sm btn-danger btn" href="">Delete</a>
-                                </td>
+                                    <td>{{$post->title}}</td>
+                                    <td>{{$post->description}}</td>
+                                    <td>{{$post->category_id}}</td>
+                                    <td>{{date('d-m-Y', strtotime($post->created_at))}}</td>
+                                    <td>
+                                        <a class="btn-sm btn-success btn" href="">Show</a>
+                                        <a class="btn-sm btn-primary btn" href="">Edit</a>
+                                        <a class="btn-sm btn-danger btn" href="">Delete</a>
+                                    </td>
                             </tr>
-                        <tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
