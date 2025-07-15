df498f6835a47a6a0a57278c22583db382155b2b
Daniel <daniel.olivqueiroz@outlook.com>
2025-07-15 09:22:56 -0300

    feat: global middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -30,3 +30,7 @@ Route::get('/posts/{id}/restore', [PostController::class, 'restore'])->name('pos
 Route::delete('/posts/{id}/force-delete', [PostController::class, 'forceDelete'])->name('posts.force_delete');
 
 Route::resource('posts', PostController::class);
+
+Route::get('/unavailable', function() {
+    return view('unavailable');
+})->name('unavailable');
