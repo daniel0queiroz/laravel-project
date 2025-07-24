9d8939d4673adb39fd43b78c308ef3b267733612
Daniel <daniel.olivqueiroz@outlook.com>
2025-07-24 09:33:39 -0300

    feat: 119 install breeze starter kit

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,17 +1,6 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ImageController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\PostController;
-use App\Mail\OrderShipped;
-use App\Models\Post;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,72 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::group(['middleware' => 'authCheck2'], function() {
-    Route::get('/dashboard', function() {
-        return view('dashboard');
-    });
-
-    Route::get('/profile', function() {
-        return view('profile');
-    });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('posts/trash', [PostController::class, 'trashed'])->name('posts.trashed');
-Route::get('/posts/{id}/restore', [PostController::class, 'restore'])->name('posts.restore');
-Route::delete('/posts/{id}/force-delete', [PostController::class, 'forceDelete'])->name('posts.force_delete');
-
-Route::resource('posts', PostController::class);
-
-Route::get('/unavailable', function() {
-    return view('unavailable');
-})->name('unavailable');
-
-Route::get('contact', function() {
-    $posts = Post::all();
-    return view('contact', compact('posts'));
-});
-
-Route::get('send-mail', function() {
-    // Mail::raw('Hello world this is a test mail', function($message) {
-    //     $message->to('test@gmail.com')->subject('hi this is a test mai');
-    // });
-
-    Mail::send(new OrderShipped);
-
-    dd('success');
-});
-
-Route::get('get-session', function(Request $request) {
-    // $data = session()->all();
-
-    $data = $request->session()->all();
-
-    // $data = $request->session()->get('_token');
-
-    dd($data);
-});
-
-Route::get('save-session', function(Request $request){
-    session(['user_id' => '123']);
-    $request->session()->put(['user_status' => 'logged_in']);
-    session(['user_ip' => '123.23.11']);
-    return redirect('get-session');
-});
-
-Route::get('destroy-session', function(Request $request) {
-    // $request->session()->forget(['user_status', 'user_ip']);
-    // session()->forget(['user_status', 'user_ip']);
-    session()->flush();
-    $request->session()->flush();
-    return redirect('get-session');
-});
-
-Route::get('flash-session', function(Request $request) {
-    $request->session()->flash('status', 'true');
-    return redirect('get-session');
-});
-
-Route::get('forget-cache', function() {
-    Cache::forget('posts');
-});
+require __DIR__.'/auth.php';
